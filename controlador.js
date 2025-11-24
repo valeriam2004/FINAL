d61720e29101e48df0e92297cf9dbe062e022605
morasantiago7085-sketch <morasantiago7085@gmail.com>
2025-11-24 15:07:22 -0500

    se hace cambio

---
+++ controlador.js
@@ -0,0 +1,20 @@
+let boton=document.getElementById("boton")
+boton.addEventListener("click",function(evento){
+    evento.preventDefault()
+
+
+    let cajaUsuario=document.getElementById("cajausuario")
+    let cajaCorreo=document.getElementById("cajacorreo")
+    let cajaCiudad=document.getElementById("cajaciudad")
+    let cajaTelefono=document.getElementById("cajatelefono")
+    let cajaContraseña=document.getElementById("cajacontraseña")                   
+    
+    let datosDelFormulario={
+        usuario:cajaUsuario.valvue,
+        correo:cajaCorreo.valvue,
+        ciudad:cajaCiudad.valvue,
+        telefono:cajaTelefono.valvue,
+        contraseña:cajaContraseña.valvue,
+    }
+
+})
